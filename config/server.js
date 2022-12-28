798fe046351c16c83f1d4983fe7705fe8ec98ce5
AntixxTechHub <info@antixxtechhub.com>
2022-12-28 17:59:07 +0530

    server.js update

--- config/server.js
+++ config/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://api.antixxtechhub.com/',
+  // url: 'https://api.antixxtechhub.com/',
+  url: env("PUBLIC_URL", "https://api.antixxtechhub.com"),
   app: {
     keys: env.array('APP_KEYS'),
   },
