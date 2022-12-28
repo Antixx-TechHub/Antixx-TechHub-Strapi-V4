6ee4c5227663f3a8ae3f0bccf35219df566ba079
AntixxTechHub <info@antixxtechhub.com>
2022-12-28 17:31:52 +0530

    Local host working

--- config/database.js
+++ config/database.js
@@ -3,11 +3,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env('DATABASE_HOST', 'containers-us-west-34.railway.app'),
-        port: env.int('DATABASE_PORT', 7958),
+      host: env('DATABASE_HOST', 'containers-us-west-28.railway.app'),
+        port: env.int('DATABASE_PORT', 6065),
         database: env('DATABASE_NAME', 'railway'),
         user: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'Lg3zUIFzqf5cRnvukScG'),
+        password: env('DATABASE_PASSWORD', 'pX3z2QPPIPBqDyWI3A6b'),
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
