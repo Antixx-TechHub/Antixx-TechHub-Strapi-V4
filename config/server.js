8a34a7d85e664b2d6299f2cb03e47bf410abfc73
AntixxTechHub <info@antixxtechhub.com>
2022-12-28 15:50:51 +0530

    Server.js update

--- config/server.js
+++ config/server.js
@@ -1,36 +1,8 @@
-// module.exports = ({ env }) => ({
-//   host: env('HOST', '0.0.0.0'),
-//   port: env.int('PORT', 1337),
-//   // url: 'https://api.antixxtechhub.com/',
-//   app: {
-//     keys: env.array('APP_KEYS'),
-//   },
-// });
-
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   url: env("PUBLIC_URL", "https://api.antixxtechhub.com/"),
-  admin: {
-    auth: {
-      secret: env("ADMIN_JWT_SECRET", "default-secret-here"),
-    },
+  app: {
+    keys: env.array('APP_KEYS'),
   },
 });
-
-// path: ./config/server.js
-
-// module.exports = ({ env }) => ({
-//   host: env('HOST', '0.0.0.0'),
-//   port: env.int('PORT', 1337),
-//   app: {
-//     keys: env.array('APP_KEYS'),
-//   },
-//   socket: '/tmp/nginx.socket', // only use if absolutely required
-//   emitErrors: false,
-//   url: env('PUBLIC_URL', 'https://api.antixxtechhub.com/'),
-//   proxy: env.bool('IS_PROXIED', true),
-//   cron: {
-//     enabled: env.bool('CRON_ENABLED', false),
-//   },
-// });
