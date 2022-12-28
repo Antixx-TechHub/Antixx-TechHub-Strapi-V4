25b96ff6676ae4874ebb78fa55a3df31ae668e6a
AntixxTechHub <info@antixxtechhub.com>
2022-12-28 15:44:46 +0530

    server.js

--- config/server.js
+++ config/server.js
@@ -7,19 +7,30 @@
 //   },
 // });
 
-// path: ./config/server.js
-
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  socket: '/tmp/nginx.socket', // only use if absolutely required
-  emitErrors: false,
-  url: env('PUBLIC_URL', 'https://api.antixxtechhub.com'),
-  proxy: env.bool('IS_PROXIED', true),
-  cron: {
-    enabled: env.bool('CRON_ENABLED', false),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("PUBLIC_URL", "https://api.antixxtechhub.com/"),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "default-secret-here"),
+    },
   },
 });
+
+// path: ./config/server.js
+
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+//   socket: '/tmp/nginx.socket', // only use if absolutely required
+//   emitErrors: false,
+//   url: env('PUBLIC_URL', 'https://api.antixxtechhub.com/'),
+//   proxy: env.bool('IS_PROXIED', true),
+//   cron: {
+//     enabled: env.bool('CRON_ENABLED', false),
+//   },
+// });
