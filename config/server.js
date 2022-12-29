480288cf7b26a4d44cdef38054b3c554de31f787
AntixxTechHub <info@antixxtechhub.com>
2022-12-30 00:03:07 +0530

    pbw

--- config/server.js
+++ config/server.js
@@ -1,16 +1,8 @@
-module.exports = ({ env }) => ({
+  module.exports = ({ env }) => ({ 
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', "https://api.antixxtechhub.com/"),
+  url: env('PUBLIC_URL', 'http://api.antixxtechhub.com'),
   app: {
     keys: env.array('APP_KEYS', ['myKeyA', 'myKeyB']),
   },
-
-  // socket: '/tmp/nginx.socket', // only use if absolutely required
-  // emitErrors: false,
-  // url: env('PUBLIC_URL', 'https://api.antixxtechhub.com'),
-  // proxy: env.bool('IS_PROXIED', true),
-  // cron: {
-  //   enabled: env.bool('CRON_ENABLED', false),
-  // },
 });
