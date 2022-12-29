91548f92bce41cea21f98c492bdb0c3d55fb40de
AntixxTechHub <info@antixxtechhub.com>
2022-12-29 23:36:12 +0530

    nginx socket added

--- config/admin.js
+++ config/admin.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT', '96ed8b33100e2b5ddedbd573a33ee2fd'),
+  },
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'a039f7bac6c23ac27c3be8415160908b'),
   },
--- config/server.js
+++ config/server.js
@@ -4,4 +4,11 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS', ['myKeyA', 'myKeyB']),
   },
+  socket: '/tmp/nginx.socket', // only use if absolutely required
+  emitErrors: false,
+  url: env('PUBLIC_URL', 'https://api.antixxtechhub.com'),
+  proxy: env.bool('IS_PROXIED', true),
+  cron: {
+    enabled: env.bool('CRON_ENABLED', false),
+  },
 });
