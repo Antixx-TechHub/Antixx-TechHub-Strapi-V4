97f24eaf5e68eb63a29eae0e358609ccad2e42c1
AntixxTechHub <info@antixxtechhub.com>
2022-12-29 23:46:22 +0530

    server pb

--- config/server.js
+++ config/server.js
@@ -1,14 +1,16 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', "https://antixxtechhub.com:1337"),
   app: {
     keys: env.array('APP_KEYS', ['myKeyA', 'myKeyB']),
   },
-  socket: '/tmp/nginx.socket', // only use if absolutely required
-  emitErrors: false,
-  url: env('PUBLIC_URL', 'https://api.antixxtechhub.com'),
-  proxy: env.bool('IS_PROXIED', true),
-  cron: {
-    enabled: env.bool('CRON_ENABLED', false),
-  },
+
+  // socket: '/tmp/nginx.socket', // only use if absolutely required
+  // emitErrors: false,
+  // url: env('PUBLIC_URL', 'https://api.antixxtechhub.com'),
+  // proxy: env.bool('IS_PROXIED', true),
+  // cron: {
+  //   enabled: env.bool('CRON_ENABLED', false),
+  // },
 });
